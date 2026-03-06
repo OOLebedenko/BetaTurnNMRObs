437eb3d0f1eb716befb3f93db90d2ef44b764873
diiana-33 <dianka_1333@mail.ru>
2026-03-06 17:45:38 +0300

    fix template consider GLY

--- scripts/calc_j_constant.py
+++ scripts/calc_j_constant.py
@@ -5,7 +5,7 @@ import pandas as pd
 from glob import glob
 from tqdm import tqdm
 
-from process_utils.calc import calc_j_c_c, calc_j_hn_ha
+from process_utils.calc import calc_j_c_c, calc_j_hn_ha, calc_j_hn_ha_gly, calc_j_c_c_gly
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Calc J(HN-HA) and J(C-C) constants')
@@ -17,10 +17,14 @@ if __name__ == '__main__':
     dihedral_csvs.sort()
 
     for dihedral_csv in tqdm(dihedral_csvs, desc="Calculation j-consts"):
+        res = os.path.basename(dihedral_csv)
         df_dihedral = pd.read_csv(dihedral_csv)
-        j_hn_ha = calc_j_hn_ha(phi_array=df_dihedral["phi"])
-        j_c_c = calc_j_c_c(phi_array=df_dihedral["phi"])
-
+        if res == "06_GLY":
+            j_hn_ha = calc_j_hn_ha_gly(phi_array=df_dihedral["phi"])
+            j_c_c = calc_j_c_c_gly(phi_array=df_dihedral["phi"])
+        else:
+            j_hn_ha = calc_j_hn_ha(phi_array=df_dihedral["phi"])
+            j_c_c = calc_j_c_c(phi_array=df_dihedral["phi"])
         os.makedirs(args.output_directory, exist_ok=True)
         pd.DataFrame({"J-HN-HA": j_hn_ha, "J-C-C": j_c_c}).to_csv(
             os.path.join(args.output_directory, os.path.basename(dihedral_csv)))
--- scripts/process_utils/calc.py
+++ scripts/process_utils/calc.py
@@ -33,7 +33,6 @@ def calc_j_hn_ha(phi_array: NDArray[np.float64]) -> NDArray[np.float64]:
     """
     ...
 
-
 def calc_j_c_c(phi_array: NDArray[np.float64]) -> NDArray[np.float64]:
     """
     Calculate J_c_c values based on the phi array.
@@ -62,5 +61,73 @@ def calc_j_c_c(phi_array: NDArray[np.float64]) -> NDArray[np.float64]:
     >>> # Continuous range example
     >>> phi = np.linspace(-180, 180, 50)
     >>> j_c_c = calc_j_c_c(phi)
+    """
+    ...
+
+def calc_j_hn_ha_gly(phi_array: NDArray[np.float64]) -> NDArray[np.float64]:
+    """
+    Calculate J_HN-HA coupling constants for glycine residues based on phi angles.
+
+    Parameters
+    ----------
+    phi_array : NDArray[np.float64]
+        Input array of phi dihedral angles (in degrees).
+        Expected to be a 1D array of float64 values in range [-180, 180].
+
+    Returns
+    -------
+    NDArray[np.float64]
+        Array of calculated J_HN-HA coupling constants (in Hz)
+        with the same shape as input.
+
+    Notes
+    -----
+    J_HN-HA represents the three-bond vicinal coupling constant between
+    amide proton and alpha proton in glycine residues. The calculation
+    uses a modified Karplus relation specific for glycine due to its
+    two alpha protons and lack of beta carbon.
+
+    Examples
+    --------
+    >>> phi = np.array([-180, -120, -60, 0, 60, 120, 180])
+    >>> j_hn_ha_gly = calc_j_hn_ha_gly(phi)
+
+    >>> # Continuous range example
+    >>> phi = np.linspace(-180, 180, 200)
+    >>> j_hn_ha = calc_j_hn_ha_gly(phi)
+    """
+    ...
+
+def calc_j_c_c_gly(phi_array: NDArray[np.float64]) -> NDArray[np.float64]:
+    """
+    Calculate J_C-C coupling constants for glycine residues based on phi angles.
+
+    Parameters
+    ----------
+    phi_array : NDArray[np.float64]
+        Input array of phi dihedral angles (in degrees).
+        Expected to be a 1D array of float64 values in range [-180, 180].
+
+    Returns
+    -------
+    NDArray[np.float64]
+        Array of calculated J_C-C coupling constants (in Hz)
+        with the same shape as input.
+
+    Notes
+    -----
+    J_C-C represents the three-bond vicinal coupling constant between
+    carbonyl carbon and alpha carbon in glycine residues. The calculation
+    uses a Karplus-type parametrization specific for glycine, accounting
+    for its unique structural features.
+
+    Examples
+    --------
+    >>> phi = np.array([-180, -135, -90, -45, 0, 45, 90, 135, 180])
+    >>> j_c_c_gly = calc_j_c_c_gly(phi)
+
+    >>> # Continuous range example
+    >>> phi = np.linspace(-180, 180, 100)
+    >>> j_c_c = calc_j_c_c_gly(phi)
     """
     ...
