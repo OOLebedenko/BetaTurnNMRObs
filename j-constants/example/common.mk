a36d336b5712fc2858b253037949162f32f15098
OOLebedenko <oolebedenko@gmail.com>
2026-03-11 15:59:32 +0300

    move j-constants analysis into a separate folder

--- j-constants/example/common.mk
+++ j-constants/example/common.mk
@@ -1,8 +1,8 @@
 SCRIPT_DIR:=../scripts/
 
 # specify path to reference structure and trajectory
-REFERENCE_PDB_PATH="../short_example_trajectory/reference.pdb"
-PATH_TO_TRAJECTORY="../short_example_trajectory"
+REFERENCE_PDB_PATH="../../short_example_trajectory/reference.pdb"
+PATH_TO_TRAJECTORY="../../short_example_trajectory"
 
 # specify trajectory setting
 TRAJECTORY_LENGTH=10
