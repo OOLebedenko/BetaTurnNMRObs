a10025cf81a46617a4b2b820572942ae3a792f3e
diiana-33 <dianka_1333@mail.ru>
2026-03-11 14:09:40 +0300

    write calc.py

--- scripts/process_utils/calc.py
+++ scripts/process_utils/calc.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy.typing import NDArray
-
+import math
 
 def calc_j_hn_ha(phi_array: NDArray[np.float64]) -> NDArray[np.float64]:
     """
@@ -31,7 +31,15 @@ def calc_j_hn_ha(phi_array: NDArray[np.float64]) -> NDArray[np.float64]:
     >>> phi = np.linspace(-180, 180, 100)
     >>> j_hn_ha = calc_j_hn_ha(phi)
     """
-    ...
+    j_hn_ha = np.array()
+    for phi in phi_array:
+        phi_rad = math.radians(phi)
+        theta_rad = phi_rad - math.radians(60)
+        j = 7.97 * math.cos(theta_rad) ** 2 - 1.26 * math.cos(theta_rad) + 0.63
+        j_hn_ha.append(j)
+    return j_hn_ha
+
+
 
 def calc_j_c_c(phi_array: NDArray[np.float64]) -> NDArray[np.float64]:
     """
@@ -62,7 +70,13 @@ def calc_j_c_c(phi_array: NDArray[np.float64]) -> NDArray[np.float64]:
     >>> phi = np.linspace(-180, 180, 50)
     >>> j_c_c = calc_j_c_c(phi)
     """
-    ...
+    j_c_c = np.array()
+    for phi in phi_array:
+        phi_rad = math.radians(phi)
+        j = 1.61 * math.cos(phi_rad) ** 2 - 0.93 * math.cos(phi_rad) + 0.55
+        j_c_c.append(j)
+    return j_c_c
+
 
 def calc_j_hn_ha_gly(phi_array: NDArray[np.float64]) -> NDArray[np.float64]:
     """
@@ -96,7 +110,13 @@ def calc_j_hn_ha_gly(phi_array: NDArray[np.float64]) -> NDArray[np.float64]:
     >>> phi = np.linspace(-180, 180, 200)
     >>> j_hn_ha = calc_j_hn_ha_gly(phi)
     """
-    ...
+    j_hn_ha = np.array()
+    for phi in phi_array:
+        phi_rad = math.radians(phi)
+        j = 8.09 * math.cos(phi_rad) ** 2 + 1.00 * math.cos(phi_rad) + 1.01
+        j_hn_ha.append(j)
+    return j_hn_ha
+
 
 def calc_j_c_c_gly(phi_array: NDArray[np.float64]) -> NDArray[np.float64]:
     """
@@ -130,4 +150,9 @@ def calc_j_c_c_gly(phi_array: NDArray[np.float64]) -> NDArray[np.float64]:
     >>> phi = np.linspace(-180, 180, 100)
     >>> j_c_c = calc_j_c_c_gly(phi)
     """
-    ...
+    j_c_c = np.array()
+    for phi in phi_array:
+        phi_rad = math.radians(phi)
+        j = 1.73 * math.cos(phi_rad) ** 2 + 1.08 * math.cos(phi_rad) + 0.28
+        j_c_c.append(j)
+    return j_c_c
